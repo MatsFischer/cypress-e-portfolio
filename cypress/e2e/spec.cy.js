08cb893b9d4182800e268513742fb55d61caeb92
mfischer <fischer@econda.de>
2023-06-14 06:01:06 +0200

    added tasks to spec file

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,36 @@
 describe('Home Page', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+  context('Header', () => {
+    it('Should have a title', () => {
+      // TODO: Does the Document have the right title?
+    })
+
+    it('Should display the correct heading', () => {
+      // TODO: Does the h1 ELement have the right text?
+    })
+
+    it('Should display a welcome message', () => {
+      // TODO: Does the welcome message paragraph have the right text?
+    })
+  })
+
+  context('Login form', () => {
+    it('Should fill in the login form and submit it', () => {
+      // TODO: Test if the user can login through entering data from the user fixture. Does the Success message appear after login?
+    })
+  })
+
+  context('Todo list', () => {
+    it('Should have a list of tasks', () => {
+      // TODO: After login - does the todo-list have the length of 3?
+    })
+  })
+
+  context('Tables', () => {
+    it('Should display user information in a table', () => {
+      // TODO: After login - does the user-table have length 4? Does the second element contain the name "John Doe"?
+    })
+
+    // TODO: *IF YOUR FAST* create a Test for the admin-table. You choose what should be tested.
   })
 })
